d3af44fd520f7b22244e8e85b89a2e74ffce2b24
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-07-28 11:36:33 -0400

    Added let todo be an array, added let deleteButton create a button, let checkButton create a button, let input be referenced todoInput from html, and const ul be referenced ul from html to final.js

---
+++ final.js
@@ -0,0 +1,5 @@
+let todo = [];
+let deleteButton = document.createElement("button");
+let checkButton = document.createElement("button");
+let input = document.getElementById("todoInput");
+const ul = document.querySelector("ul");
